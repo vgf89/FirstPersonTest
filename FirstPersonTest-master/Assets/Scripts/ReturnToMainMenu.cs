492bef13f8caa6467ed5f6b0234a6dc583a19edf
Forest Thomas <thomasforestg@gmail.com>
2016-04-03 15:50:56 -0600

    Created multiple menus with scripts to traverse the menus

---
+++ FirstPersonTest-master/Assets/Scripts/ReturnToMainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReturnToMainMenu : MonoBehaviour {
+    public GameObject currentMenu;
+    public GameObject mainMenu;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Back"))
+        {
+            currentMenu.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+    }
+}
---
+++ FirstPersonTest-master/Assets/Scripts/SwitchMenus.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchMenus : MonoBehaviour {
+    public GameObject destinationMenu;
+    public GameObject mainMenu;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void onClick()
+    {
+        mainMenu.SetActive(false);
+        destinationMenu.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
